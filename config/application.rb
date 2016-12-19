8b780514bc775cb63d2a2d3e6b26a6a2f8f9c21f
Ubuntu <ubuntu@experteese>
2016-12-19 21:55:50 +0000

    home first commit

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Expert1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.fallbacks = true
+    config.i18n.fallbacks = [:en]
+    config.i18n.enforce_available_locales = true
   end
 end
